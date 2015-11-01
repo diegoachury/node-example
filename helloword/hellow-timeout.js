3e9d3ff3ddc60d2b9d6a115bdd9ac72f5589c994
diegoachury <tiran_o@hotmail.com>
2015-11-01 11:15:31 -0500

    ejecutar setTimeout

---
+++ helloword/hellow-timeout.js
@@ -0,0 +1,18 @@
+var http = require('http');
+
+http.createServer(function( request, response){
+	response.writeHead(200);
+	response.write("Dog is running.");
+
+	setTimeout(function(){
+		response.write("Dog is done");
+		response.end();
+	}, 5000);
+
+}).listen(8080);
+
+
+ /* setTimeout
+	es un metodo que es utilizado para ejecutar una funcion en un tiempo determinado. en este caso se ecuentra 5000
+	milisegundos que equivalen a 5 segundos.
+ */
--- helloword/hellow.js
+++ helloword/hellow.js
@@ -1,4 +1,4 @@
-var http = requiere('http');
+var http = require('http');
 
 http.createServer(function( request, response){
 	response.writeHead(200);
